39d0f334a045aabccfafceb2d9cd9dbed05e4084
Rick1216820014 <1216820014@qq.com>
2024-01-12 16:40:57 +0800

    广告删除

---
+++ gvb_server/api/advert_api/advert_remove.go
@@ -0,0 +1,29 @@
+package advert_api
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gvb_server/global"
+	"gvb_server/models"
+	"gvb_server/models/res"
+)
+
+func (AdvertApi) AdvertRemoveView(c *gin.Context) {
+	var cr models.RemoveRequest
+	err := c.ShouldBindJSON(&cr)
+	if err != nil {
+		res.FailWithCode(res.ArgumentError, c)
+		global.Log.Error(err)
+		return
+	}
+
+	var advertList []models.AdvertModel
+	//Find()第一个参数是指针类型，需要传入一个切片的指针，否则会报错：panic recovered:reflect: reflect.Value.Set using unaddressable value
+	count := global.DB.Find(&advertList, cr.IDList).RowsAffected
+	if count == 0 {
+		res.FailWithMessage("广告不存在", c)
+		return
+	}
+	global.DB.Delete(&advertList)
+	res.OkWithMessage(fmt.Sprintf("删除%d个广告", count), c)
+}
