4e9dbf2d0e4e55940341a588f067914539b33a92
Rick1216820014 <1216820014@qq.com>
2024-01-19 16:15:51 +0800

    菜单upodate第二次修复bug

--- gvb_server/api/menu_api/menu_update.go
+++ gvb_server/api/menu_api/menu_update.go
@@ -49,72 +49,74 @@ func (MenuApi) MenuUpdateView(c *gin.Context) {
 				global.Log.Error(err)
 				return
 			}
-		}
-		create_mbsList := []models.MenuBannerModel{}
-		update_mbsList := []models.MenuBannerModel{}
-		delete_mbsList := []models.MenuBannerModel{}
-		for i := 0; i < len(mbsList); i++ {
+		} else {
+			create_mbsList := []models.MenuBannerModel{}
+			update_mbsList := []models.MenuBannerModel{}
+			delete_mbsList := []models.MenuBannerModel{}
+			for i := 0; i < len(mbsList); i++ {
 
-			for j := 0; j < len(bannerMenuList); j++ {
-				//bannerid相同但是sort不同，记录在更新切片中
-				if mbsList[i].BannerID == bannerMenuList[j].BannerID &&
-					mbsList[i].Sort != bannerMenuList[j].Sort {
-					update_mbsList = append(update_mbsList, models.MenuBannerModel{
-						MenuID:   id,
-						BannerID: mbsList[i].BannerID,
-						Sort:     mbsList[i].Sort,
-					})
-					break
-				} else if mbsList[i].BannerID == bannerMenuList[j].BannerID &&
-					mbsList[i].Sort == bannerMenuList[j].Sort {
-					//防止出现最后一次循环找到相同的切片，并且保存到插入切片中
-					break
+				for j := 0; j < len(bannerMenuList); j++ {
+					//bannerid相同但是sort不同，记录在更新切片中
+					if mbsList[i].BannerID == bannerMenuList[j].BannerID &&
+						mbsList[i].Sort != bannerMenuList[j].Sort {
+						update_mbsList = append(update_mbsList, models.MenuBannerModel{
+							MenuID:   id,
+							BannerID: mbsList[i].BannerID,
+							Sort:     mbsList[i].Sort,
+						})
+						break
+					} else if mbsList[i].BannerID == bannerMenuList[j].BannerID &&
+						mbsList[i].Sort == bannerMenuList[j].Sort {
+						//防止出现最后一次循环找到相同的切片，并且保存到插入切片中
+						break
+					}
+					//如果确定数据库中没有这个图片，记录在插入切片中
+					if j == len(bannerMenuList)-1 {
+						create_mbsList = append(create_mbsList, models.MenuBannerModel{
+							MenuID:   id,
+							BannerID: mbsList[i].BannerID,
+							Sort:     mbsList[i].Sort,
+						})
+					}
 				}
-				//如果确定数据库中没有这个图片，记录在插入切片中
-				if j == len(bannerMenuList)-1 {
-					create_mbsList = append(create_mbsList, models.MenuBannerModel{
-						MenuID:   id,
-						BannerID: mbsList[i].BannerID,
-						Sort:     mbsList[i].Sort,
-					})
+
+			}
+			//反向查找，数据库中的图片是否包含在新的图片列表中，如果没有就加入删除切片中
+			for i := 0; i < len(bannerMenuList); i++ {
+
+				for j := 0; j < len(mbsList); j++ {
+					if mbsList[j].BannerID == bannerMenuList[i].BannerID &&
+						mbsList[j].Sort == bannerMenuList[i].Sort {
+						break
+					} else if mbsList[j].BannerID == bannerMenuList[i].BannerID &&
+						mbsList[j].Sort != bannerMenuList[i].Sort {
+						break
+					}
+					if j == len(mbsList)-1 {
+						delete_mbsList = append(delete_mbsList, models.MenuBannerModel{
+							MenuID:   id,
+							BannerID: bannerMenuList[i].BannerID,
+							Sort:     bannerMenuList[i].Sort,
+						})
+					}
 				}
 			}
+			if len(create_mbsList) != 0 {
+				global.DB.Debug().Create(&create_mbsList)
+			}
 
-		}
-		//反向查找，数据库中的图片是否包含在新的图片列表中，如果没有就加入删除切片中
-		for i := 0; i < len(bannerMenuList); i++ {
-
-			for j := 0; j < len(mbsList); j++ {
-				if mbsList[j].BannerID == bannerMenuList[i].BannerID &&
-					mbsList[j].Sort == bannerMenuList[i].Sort {
-					break
-				} else if mbsList[j].BannerID == bannerMenuList[i].BannerID &&
-					mbsList[j].Sort != bannerMenuList[i].Sort {
-					break
+			if len(update_mbsList) != 0 {
+				for _, data := range update_mbsList {
+					global.DB.Debug().Model(&models.MenuBannerModel{}).Where("menu_id = ? and banner_id = ?", data.MenuID, data.BannerID).Updates(data)
 				}
-				if j == len(mbsList)-1 {
-					delete_mbsList = append(delete_mbsList, models.MenuBannerModel{
-						MenuID:   id,
-						BannerID: bannerMenuList[i].BannerID,
-						Sort:     bannerMenuList[i].Sort,
-					})
+
+			}
+			if len(delete_mbsList) != 0 {
+				for _, data := range delete_mbsList {
+					global.DB.Debug().Where("menu_id = ? and banner_id = ?", data.MenuID, data.BannerID).Delete(&models.MenuBannerModel{}, data)
 				}
 			}
 		}
-		if len(create_mbsList) != 0 {
-			global.DB.Debug().Create(&create_mbsList)
-		}
-
-		if len(update_mbsList) != 0 {
-			for _, data := range update_mbsList {
-				global.DB.Debug().Model(&models.MenuBannerModel{}).Where("menu_id = ? and banner_id = ?", data.MenuID, data.BannerID).Updates(data)
-			}
-
-		}
-		if len(delete_mbsList) != 0 {
-			global.DB.Debug().Delete(&models.MenuBannerModel{}, delete_mbsList)
-		}
-
 	} else {
 		//如果更新的是空图片列表，就是删除关联的所有图片
 		err = global.DB.Take(&menuModel, id).Error
@@ -133,7 +135,5 @@ func (MenuApi) MenuUpdateView(c *gin.Context) {
 		res.FailWithMessage("修改菜单失败", c)
 		return
 	}
-
 	res.OkWithMessage("修改菜单成功", c)
-
 }
